f9c8bb670aa4ecc37155026cec698bc044d512d1
ATomShermental <nike123a44@gmail.com>
2023-02-28 16:50:13 +0300

    add changes

---
+++ osm-data-preparation/bonus-custom-parser/XML_Parser/src/main/java/org/example/Bounds.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Bounds {
+    private long minlat;
+    private long minlon;
+    private long maxlat;
+    private long maxlon;
+}
---
+++ osm-data-preparation/bonus-custom-parser/XML_Parser/src/main/java/org/example/OSM.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+
+public class OSM {
+    private List<Node> nodes;
+    private List<Way> ways;
+    private List<Relation> relations;
+
+}
